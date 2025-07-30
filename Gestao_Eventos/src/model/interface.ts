02027f47ff580b36e5d61c00e2e6743f8931773e
Sofia Josafá <sofiajosafa19@gmail.com>
2025-07-30 14:15:29 -0300

    Delete Gestao_Eventos directory

--- Gestao_Eventos/src/model/interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IEvento {
-    id: String
-    nome: String
-    data: String
-    local: String
-    capacidadeMaxima: number  
-    participantesAtuais: number 
-    status: String
-  }
