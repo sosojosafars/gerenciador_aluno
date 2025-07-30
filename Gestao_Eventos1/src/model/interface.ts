0dc8ca36b0bed0c2fbcc7d05bacce6be2450a70e
Sofia Josafá <sofiajosafa19@gmail.com>
2025-07-30 11:45:31 -0300

    Add files via upload

---
+++ Gestao_Eventos1/src/model/interface.ts
@@ -0,0 +1,9 @@
+export interface IEvento {
+    id: String
+    nome: String
+    data: String
+    local: String
+    capacidadeMaxima: number  
+    participantesAtuais: number 
+    status: String
+  }
